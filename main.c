7bcc67ea971e5fea2ac82ae2266441588aa9beb5
James Zhao <zhaoyafei0210@gmail.com>
2016-08-23 01:21:12 +0800

    print augments list

--- main.c
+++ main.c
@@ -6,6 +6,15 @@ int main(int argc, char* argv[])
     
     printf("This message is added through github website!\n");
 
+
+    printf("Print Arguments List:\n")
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%d]:　%s\n", i, argv[i]);
+    }
+
+    printf("\n");
+
     for (int i = 0; i < 10; i++)
     {
         printf("This is just a loop test, iter = %d\n", i);
