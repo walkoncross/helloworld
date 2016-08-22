53ae141c06f5261144cfff01c2ba742e59e2904c
James Zhao <zhaoyafei0210@gmail.com>
2016-08-23 00:55:49 +0800

    Add a for-loop test

--- main.c
+++ main.c
@@ -5,5 +5,11 @@ int main(int argc, char* argv[])
     printf("Hello World!\n");
     
     printf("This message is added through github website!\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("This is just a loop test, iter = %d\n", i);
+    }
+
     return 1;
 }
